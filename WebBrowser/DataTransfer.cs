7f5df6dab24b78a3423d184a69029c6b763d2181
AkashDayalan <akashdaya22@gmail.com>
2022-01-22 02:05:42 +0530

    Add project files.

---
+++ WebBrowser/DataTransfer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Xml.Dom;
+using Windows.Storage;
+
+namespace WebBrowser
+{
+    public class DataTransfer
+    {
+        //the file name
+        string filename = "settings.xml";
+
+        public async void SaveSearchTerm(string SearchTerm, string title, string url)
+        {
+            var doc = await DocumentLoad().AsAsyncOperation(); //loads the Xml file
+
+            var history = doc.GetElementsByTagName("history");
+
+            XmlElement elSearchTerm = doc.CreateElement("searchterm");
+            XmlElement elSiteName = doc.CreateElement("sitename");
+            XmlElement elUrl = doc.CreateElement("url");
+
+            var historyItem = history[0].AppendChild(doc.CreateElement("historyItem"));
+
+            historyItem.AppendChild(elSearchTerm);
+            historyItem.AppendChild(elSiteName);
+            historyItem.AppendChild(elUrl);
+
+            elSearchTerm.InnerText = SearchTerm;
+            elSiteName.InnerText = title;
+            elUrl.InnerText = url;
+
+            SaveDocument(doc);
+        }
+
+        private async Task<XmlDocument> DocumentLoad()
+        {
+            XmlDocument result = null;
+
+            await Task.Run(async () =>
+            {
+                var file = await ApplicationData.Current.LocalFolder.GetFileAsync(filename);
+                XmlDocument doc = await XmlDocument.LoadFromFileAsync(file);
+                result = doc;
+            });
+
+            return result;
+        }
+
+        private async void SaveDocument(XmlDocument doc)
+        {
+            var file = await ApplicationData.Current.LocalFolder.GetFileAsync(filename);
+            await doc.SaveToFileAsync(file);
+        }
+    }
+}
---
+++ WebBrowser/MainPage.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WebBrowser
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            DataAccess dataAccess = new DataAccess();
+            dataAccess.CreateSettingsFile();
+        }
+
+        private void backBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (webBrowser.CanGoBack)
+            {
+                webBrowser.GoBack();
+            }
+        }
+
+        private void frdBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (webBrowser.CanGoForward)
+            {
+                webBrowser.GoForward();
+            }
+        }
+
+        private void SearchBar_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                Search();
+            }
+        }
+
+        private void Search()
+        {
+            webBrowser.Source = new Uri("https://www.google.com/search?q=" + SearchBar.Text);
+        }
+
+        private void refreshBtn_Click(object sender, RoutedEventArgs e)
+        {
+            webBrowser.Refresh();
+        }
+
+        private void homeBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void settingsMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(SettingsPage));
+        }
+
+        private void MainBrowserWindow_Loading(FrameworkElement sender, object args)
+        {
+
+        }
+
+        private void webBrowser_Loading(FrameworkElement sender, object args)
+        {
+            statusText.Text = webBrowser.Source.AbsoluteUri;
+        }
+
+        private void webBrowser_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            browserProgress.IsEnabled = false;
+            browserProgress.IsIndeterminate = false;
+            try
+            {
+                statusText.Text = webBrowser.Source.AbsoluteUri;
+                AppTitle.Text = "OneWebBrowser" + "  " + webBrowser.DocumentTitle;
+
+                DataTransfer dataTransfer = new DataTransfer();
+                if (!string.IsNullOrEmpty(SearchBar.Text))
+                {
+                    dataTransfer.SaveSearchTerm(SearchBar.Text, webBrowser.DocumentTitle, webBrowser.Source.AbsoluteUri);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void webBrowser_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            browserProgress.IsEnabled = true;
+            browserProgress.IsIndeterminate = true;
+        }
+    }
+}
